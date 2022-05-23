8f04e8e5cec2218570e785e457feee6c0a0343f0
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2022-05-23 20:12:55 +0900

    rr +A

--- 소스.c
+++ 소스.c
@@ -1,142 +1,143 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define MAX_SIZE 99
-#define MAX_TIME_SIZE 30
-#define MAX_SERVICETIME_SIZE 10
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-struct queqe {
-	int proId;
-	int arrTime;
-	int serviceTime;
-	int checkCom;
-};
+#define MAX_PROCESS   100
+#define MAX_QUEUE  100
 
-void main() {
-	struct queqe z[MAX_SIZE];
-	int a, b, c, rs = 1;
-	int flag = 1, flag2 = 1;
-	int q = 1;
+
+
+typedef int(*comp)(const void*, const void*);
+//tail..........head
+typedef struct process {
+	int pid; //프로세스번호
+	int ar_t; //도착시간
+	int ser_t; //수행시간
+	bool is_finished; //종료판단변수
+	int remain_time; //수행후 남아있는 시간
+	int waiting_time; //대기시간
+	int end_time; //종료시간
+	int put; //프로세스 실행 판단 변수(0or1)
+	int end_point;
+} process;
+
+process g_process[MAX_PROCESS];
+process* prc_queue[MAX_QUEUE] = { NULL };
+int q_head = 0;
+int q_tail = -1;
+int pr_cnt = -1; //프로세스 현재 번호
+
+//RR 알고리즘
+void RR(int cnt, process s[]) { //프로세스 개수 cnt
+	//printf("%d \n",g_process[1].remain_time);
+	
+	int t = 0, i =0 , end_cnt;
+	int flag = 1;
+	int time_q = 1, time = 0 ,q = 1 ;
+
+	int exit = 0; //종료 프로세스 개수
+	int average_wait = 0;
+	double average_banhwan = 0;
+
+	for (i = 0; i < cnt; i++) {
+		g_process[i].end_point = 1;
+	}
+
+	end_cnt = cnt;
 
 	while (flag) {
-		printf("Id");
-		scanf("%d", &a);
-		if (a != 0) {
-			printf("arr");
-			scanf("%d", &b);
-			printf("ser");
-			scanf("%d", &c);
-			z[rs].proId = a;
-			z[rs].arrTime = b;
-			z[rs].serviceTime = c;
-			z[rs].checkCom = 1;
-			rs++;
-		}
-		else {
-			flag = 0;
-		}
-	}
-	for (int i = 1; i <= rs - 1; i++) {
-		printf("%d  %d  %d\n", z[i].proId, z[i].arrTime, z[i].serviceTime);
-	}
-
-	printf("\nRR\n");
-	int comfile = rs - 1;
-
-	while (flag2) {
-		int cnt = 0;
-		int j = 1;
-		int sel5;
-
+		for (i = 0; i < cnt; i++) {
+			printf("\n_______service Time : %d_________\n\n",time+1); // 삭제
 		
 
-		// Service Time q-> Servie start
-		for (j = j; j < rs; j++) {
+			if (g_process[i].remain_time > 0 &&g_process[i].ar_t <= time) {
+				printf("========ID : %d========\n",g_process[i].pid);
+				printf("수행전 남아있는 시간 %d\n",g_process[i].remain_time);
+				g_process[i].remain_time -= 1;
+				printf("수행후 남아있는 시간 %d\n", g_process[i].remain_time);
+				time++;
 
-			// comfile start
-			//printf("comfile %d\n", comfile);
-
-			if (z[j].checkCom == 0) {
-				printf("나오는데? %d\n",comfile);
-				comfile -= 1;
-			}
-
-			/*
-			for (int k = 1; k < rs; k++) {
-				if (comfile == 0) {
-					flag2 = 0;
+				/*ch*/
+				if ((g_process[i].remain_time == 0) && (g_process[i].end_point == 1)) {
+					printf("Process ID : %d 완료\n", g_process[i].pid);
+					g_process[i].end_time = time;
+					g_process[i].end_point = 0;
+					end_cnt -= 1;
 				}
-				else if (z[k].checkCom == 0) {
-					comfile = comfile - 1;
-				}
-				else {
-					comfile = rs - 1;
-					break;
-				}
+				//chend
 
-				printf("comfile %d", comfile);
-			}*/
-			// comfile end
 
-			if (z[j].serviceTime == 0) {
-				printf("Process ID : %d 는 완료했습니다\n", z[j].proId);
-				z[j].checkCom = 0;
 			}
-			else if (z[j].serviceTime < 0) {
-				printf("ERROR 다시확인하시오\n");
-				break;
-			}
-			else {
-				printf("ID : %d\n", z[j].proId);
-				printf("남은 서비스 시간 : %d\n", z[j].serviceTime);
-				z[j].serviceTime -= q;
-				printf("실행 후 남은 서비스 시간 : %d\n", z[j].serviceTime);
+
+			else if (end_cnt == 0) {
+				flag = 0;
 			}
 
 		}
-		// Service Time q-> Servie END}
-		
 
-		
-		printf("계속하려면 1");
-		scanf("%d", &sel5);
-		if (sel5 == 1) {
-			continue;
-		}
-		else {
-			flag2 = 0;
-		}
-		
 	}
+	for (i = 0; i < cnt; i++) {
+		printf("%5d%15d%17d%15d%15d \n", g_process[i].pid, g_process[i].ar_t, g_process[i].ser_t,
+		g_process[i].end_time , g_process[i].end_time-g_process[i].ar_t);
+	}
+
+	printf("\n평균 대기시간: %d\n", average_wait / cnt);
+	printf("\n평균 반환시간: %.2f\n", average_banhwan / cnt);
+}
+int main()
+{
+	FILE* file = NULL;
+	char c[MAX_PROCESS];
+	char* a, * b = NULL;
+
+	process copy_sys[MAX_QUEUE] = { 0, };
+
+	int count = 0, i;
+	int max_wait_index = 0, one_q = 1, exit = 0;
+
+	memset(g_process, 0, sizeof(g_process));
+
+	//파일 읽어오기
+	file = fopen("input.txt", "r");
+	if (file == NULL) {
+		printf("파일이 존재하지 않습니다.");
+		return -1;
+	}
+	while (fgets(c, MAX_PROCESS, file) != NULL) {
+		if (c[0] == '\n') continue;
+
+		a = strtok(c, ",");
+		if (*a == '0') {
+			break;
+		}
+		g_process[count].pid = atoi(a);
+
+		a = strtok(NULL, ",");
+		g_process[count].ar_t = atoi(a);
+		a = strtok(NULL, ",");
+		g_process[count].remain_time = g_process[count].ser_t = atoi(a);
+		g_process[count].waiting_time = 0;
+		count++;
+
+	}
+	fclose(file); //파일 닫기
+	printf("input.txt\n");
+	//input.txt 파일 정보 출력
+	for (i = 0; i < count; i++) {
+		copy_sys[i] = g_process[i];
+		//PID, 도착시간, 수행시간
+		printf("%d %d %d \n", g_process[i].pid, g_process[i].ar_t, g_process[i].ser_t);
+	}
+	puts("===============================================");
+
+
+	printf("\n<RR 스케줄링>\n\n");
+	printf("프로세스 id\t 도착시간\t 서비스 시간\t 종료 시간\t 반환 시간\t 정규화된 반환 시간\n");
+	printf("=======================================================================================================\n");
+	RR(count, copy_sys);
+	printf("=======================================================================================================\n");
 
 
 }
-
-
-
-
-/*
-int a,b,c,flag=1;
-
-while(flag){
-	scanf("%d", &a);
-	if (a != 0) {
-		scanf("%d", &b);
-		scanf("%d", &c);
-		int save[MAX_ID_SIZE] = a, b, c;
-	}
-	else {
-		break;
-	}
-
-}
-printf("%d  %d  %d",);
-*/
-
-/*
-		if (cnt == rs - 1) {
-			flag2 = 0;
-			for (int i = 1; i <= rs - 1; i++) {
-				// 만약 service time 이 0 이되면 cnt 스택을 쌓고 이 스택+1 rs와 같아지면 Break 하면 되겠군
-				printf("%d  %d  %d\n", z[i].proId, z[i].arrTime, z[i].serviceTime);
-			}
-		}*/
